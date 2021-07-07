698c17a9ba81099b9cfe9e72e856626007419902
Roman Panchenko <timberspirit@gmail.com>
2021-07-08 01:50:40 +0300

    prepared deploy project to heroku

--- frontend/app.js
+++ frontend/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Table from './src/components/Table.jsx'
+
 window.addEventListener('load', () => {
     const wrapper = document.getElementById('tablewrapper');
     ReactDOM.render(
